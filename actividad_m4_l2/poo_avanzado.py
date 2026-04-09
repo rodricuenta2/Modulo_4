902914af74b4c28600e22d3c8393c15b351241bd
Rodrigo <rodrigo.estroz+test@gmail.com>
2026-04-09 16:24:49 +0000

    solo item 1 y 2, el resto sin lograr realizar

--- actividad_m4_l2/poo_avanzado.py
+++ actividad_m4_l2/poo_avanzado.py
@@ -11,11 +11,31 @@ class Libro:
         self.anio_publicacion = anio_publicacion
 
 #• Crea un método mostrar_info() que imprima los datos del libro.
-    def mostrar_info(self):
+    def mostrar_info(self): # se crea metodo mostrar_info
         print(f"Libro: {self.titulo}")
         print(f"Autor: {self.autor}")
         print(f"Año de publicación: {self.anio_publicacion}")
 
 info_libros = Libro("Romeo y Julieta", "William Shakespeare", 1597)
 
-info_libros.mostrar_info()
+info_libros.mostrar_info() # se llama a metodo mostrar_info
+
+#2. Métodos accesadores y mutadores
+
+#• Implementa métodos get_titulo() y set_titulo(nuevo_titulo) para acceder y modificar el 
+#atributo titulo.
+
+def get_titulo(self):
+        return self._titulo
+
+def set_titulo(self, nuevo_titulo):
+       
+    
+#• Repite lo mismo para el atributo anio_publicacion.
+
+    def anio_publicacion(self):
+        return self._anio_publicacion
+
+    def set_anio_publicacion(self, nuevo_anio_publicacion):
+       
+#• Usa estos métodos para modificar el título de un libro desde el programa principal.
